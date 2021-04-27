f234f2a66cd62bb07c4f7b9562a915dea02ed9e7
Your Name <you@example.com>
2021-04-27 11:12:38 +0700

    update admin_mem

--- resources/views/admin/admin_mem/list.blade.php
+++ resources/views/admin/admin_mem/list.blade.php
@@ -1,19 +1,19 @@
 @extends('admin.layouts.master')
 @section('content')
-<!-- main content - noi dung chinh trong chu -->
-	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+<!-- main content -->
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
 				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-				<li class="active">Quản trị viên</li>
+				<li class="active">Administrator</li>
 			</ol>
 		</div><!--/.row-->
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="panel-heading">
-					Danh sách quản trị viên						
+					List Of Administrators
 				</div>
-				<div class="panel panel-default">					
+				<div class="panel panel-default">
 					@if (count($errors) > 0)
 					    <div class="alert alert-danger">
 					        <ul>
@@ -25,7 +25,7 @@
 					    @elseif (Session()->has('flash_level'))
 					    	<div class="alert alert-success">
 						        <ul>
-						            {!! Session::get('flash_massage') !!}	
+						            {!! Session::get('flash_massage') !!}
 						        </ul>
 						    </div>
 						@endif
@@ -33,43 +33,43 @@
 						<div class="table-responsive">
 							<table class="table table-hover">
 								<thead>
-									<tr>										
-										<th>ID</th>										
-										<th>Tên quản trị</th>
+									<tr>
+										<th>ID</th>
+										<th>Name Admin</th>
 										<th>Email</th>
-										<th>Quyền</th>										
+										<th>Permission</th>
 										<th>Action</th>
 									</tr>
 								</thead>
 								<tbody>
-									
+
 										<tr>
 											<td>{{$data->id}} </td>
 											<td>{{$data->name}} </td>
 											<td>{{$data->email}}</td>
 											<td>
 												@if($data->level==100)
-													<span style="color:#d35400;">Quản trị hệ thống</span>
+													<span style="color:#d35400;">System Management</span>
 												@else
-													<span style="color:#27ae60;">Quản trị viên</span>
+													<span style="color:#27ae60;">Administrator</span>
 												@endif
-											</td>											
+											</td>
 											<td>
-												<a href="" title="Chi tiết"> Cập nhật</a> &nbsp;
-												
-												<a href=""  title="Xóa" onclick="return xacnhan('Xóa danh mục này ?')">Xóa bỏ</a>
-												
+												<a href="" title="Chi tiết">Update</a> &nbsp;
+
+												<a href=""  title="Xóa" onclick="return confirm('Xóa danh mục này ?')">Delete</a>
+
 											</td>
 										</tr>
-															
+
 								</tbody>
 							</table>
 						</div>
-						
+
 					</div>
 				</div>
 			</div>
-		</div><!--/.row-->		
+		</div><!--/.row-->
 	</div>	<!--/.main-->
 <!-- =====================================main content - noi dung chinh trong chu -->
 @endsection
